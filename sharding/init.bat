07508ac65b8bcb737d920ad218297525e65bc5d9
odeloen <odeloen.nakmus@gmail.com>
2019-11-18 18:13:25 +0700

    Adding readme

--- sharding/init.bat
+++ sharding/init.bat
@@ -1,19 +1,19 @@
 REM create docker network
-REM docker network create --subnet=192.168.0.0/22 mdbnet
+REM docker network create --subnet=192.168.0.0/22 dbnet
 
 REM create and run docker instance
-docker run -d --net mdbnet --ip 192.168.1.1 -p 37019:27019 --name config-1 --hostname config-1 mongo:4.2 --replSet config-conf --configsvr
+docker run -d --net dbnet --ip 192.168.1.1 -p 37019:27019 --name config-1 --hostname config-1 mongo:4.2 --replSet config-conf --configsvr
 
-docker run -d --net mdbnet --ip 192.168.1.2 -p 37029:27019 --name config-2 --hostname config-2 mongo:4.2 --replSet config-conf --configsvr
+docker run -d --net dbnet --ip 192.168.1.2 -p 37029:27019 --name config-2 --hostname config-2 mongo:4.2 --replSet config-conf --configsvr
 timeout /t 20 /nobreak > nul
 
 docker exec -it config-1 mongo -port 27019 --eval "rs.initiate({ _id: 'config-conf', members: [{ _id: 0, host: '192.168.1.1:27019' }, { _id: 1, host: '192.168.1.2:27019' }]});"
 
-docker run -d --net mdbnet --ip 192.168.2.1 -p 47018:27018 --name shard-1 --hostname shard-1 mongo:4.2 --replSet shard-1-rs --shardsvr
+docker run -d --net dbnet --ip 192.168.2.1 -p 47018:27018 --name shard-1 --hostname shard-1 mongo:4.2 --replSet shard-1-rs --shardsvr
 
-docker run -d --net mdbnet --ip 192.168.2.2 -p 47028:27018 --name shard-2 --hostname shard-2 mongo:4.2 --replSet shard-2-rs --shardsvr
+docker run -d --net dbnet --ip 192.168.2.2 -p 47028:27018 --name shard-2 --hostname shard-2 mongo:4.2 --replSet shard-2-rs --shardsvr
 
-docker run -d --net mdbnet --ip 192.168.2.3 -p 47038:27018 --name shard-3 --hostname shard-3 mongo:4.2 --replSet shard-3-rs --shardsvr
+docker run -d --net dbnet --ip 192.168.2.3 -p 47038:27018 --name shard-3 --hostname shard-3 mongo:4.2 --replSet shard-3-rs --shardsvr
 timeout /t 20 /nobreak > nul
 
 docker exec -it shard-1 mongo -port 27018 --eval "rs.initiate({ _id: 'shard-1-rs', members: [{ _id: 0, host: '192.168.2.1:27018' }]})"
@@ -22,7 +22,7 @@ docker exec -it shard-2 mongo -port 27018 --eval "rs.initiate({ _id: 'shard-2-rs
 
 docker exec -it shard-3 mongo -port 27018 --eval "rs.initiate({ _id: 'shard-3-rs', members: [{ _id: 2, host: '192.168.2.3:27018' }]})"
 
-docker run -d --net mdbnet --ip 192.168.0.2 -p 27017:27017 --name router --hostname router mongo:4.2 mongos --configdb config-conf/192.168.1.1:27019,192.168.1.2:27019 --bind_ip_all
+docker run -d --net dbnet --ip 192.168.0.2 -p 27017:27017 --name router --hostname router mongo:4.2 mongos --configdb config-conf/192.168.1.1:27019,192.168.1.2:27019 --bind_ip_all
 
 timeout /t 20 /nobreak > nul
 REM configure sharding cluster
